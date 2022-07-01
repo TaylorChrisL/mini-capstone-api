b12807ad054617b33b279bee3642403ea22223db
Chris Taylor <ctaylor534@gmail.com>
2022-07-01 10:54:30 -0700

    updates controller to standard conventions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,15 +1,25 @@
 class ProductsController < ApplicationController
 
-  def show_all
+  def index
     products = Product.all
     render json: products.as_json
   end
 
-  def show_product
+  def show
     product = Product.find_by(id: params["id"])
-    render json: product
+    render json: product.as_json
   end
 
+  def create
+    product = Product.new(
+      name: "Kangaroo"
+      price: 589
+      image_url: "https://i.pinimg.com/originals/e6/a3/7e/e6a37e83aa99dbb409e54767d7ea9aa7.jpg"
+      description: "A boxing Kangaroo straight from the outback (Boxing gloves included)"
+    )
+
+    product.save
+    render :json product.as_json
 
 
 end
