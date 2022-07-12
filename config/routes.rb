84e7424d4d9f5af8bcf8075de433f0b63c796280
Chris Taylor <ctaylor534@gmail.com>
2022-07-12 10:26:01 -0700

    Adds controller and routing for post

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :products, defaults: { format: :json }
   resources :suppliers, defaults: { format: :json }
   resources :images, defaults: { format: :json }
+
+  post "/users" => "users#create" default: { fortmat: :json }
   
 end
