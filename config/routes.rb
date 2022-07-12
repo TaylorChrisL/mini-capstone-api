993653786826e8787ea87f51bfd4322e6f3bc0bd
Chris Taylor <ctaylor534@gmail.com>
2022-07-12 10:35:14 -0700

    fixes validation, adds Post request to create user

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :suppliers, defaults: { format: :json }
   resources :images, defaults: { format: :json }
 
-  post "/users" => "users#create" default: { fortmat: :json }
+  post "/users" => "users#create", default: { fortmat: :json }
   
 end
